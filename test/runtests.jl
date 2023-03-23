56a05102386fcea34cbd1d2417ead1c6a138ed10
biplab37 <biplabmahato37@gmail.com>
2023-03-23 15:35:05 +0100

    fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,11 +16,11 @@ end
 
 @testset "Scalar Channel (Mean Field)" begin
     @testset "σ1" begin
-        @test σ1(0.01,0.0) ≈ 1.0 atol=1e-4
-        @test σ1(0.01,0.0,Parameters(κ=0.01)) > 1.0
-        @test σ1(0.1,0.0)  < 1.0
-        @test σ1(0.1,0.0) > σ1(0.1,0.1,Parameters())
-        @test σ1(0.01,0.0,Parameters(M=M0)) ≈ M0 atol=1e-4
+        @test σ1(0.01,0.0) > 1.0 
+        @test σ1(0.01,0.0,Parameters(κ=0.0)) ≈ 1.0 atol=1e-4
+        @test σ1(0.1,0.0,Parameters(κ=0.0))  < 1.0
+        @test σ1(0.1,0.0) > σ1(0.1,0.1,Parameters(κ = 0.0))
+        @test σ1(0.01,0.0,Parameters(M=M0,κ = 0.0)) ≈ M0 atol=1e-4
     end
 end
 
